320a09f2e6826b07d70d59cc7ce8d318a49f0d9b
Ckrenzer <crkrenz72@gmail.com>
2021-02-05 18:43:05 -0500

    Made a script which will optionally clean the csv's contents

---
+++ La Junta cleaning.R
@@ -0,0 +1,17 @@
+# This script is optional, though it will simplify your analysis
+
+# There are two primary goals with this script:
+#   1. Add column names to the csv
+#   2. Organize types of cattle into a small number of distinct groups
+
+# goal 1 is a piece of cake, but goal 2 is easier said than done.
+# You can analyze some of my judgment calls and decide for
+# yourself what needs to be done to group the different types
+# of livestock.
+
+# Should the header have been made beforehand and
+# therefore not need this code? Probably. That sounds
+# like a headache to implement if the file is already
+# in your system, so a separate script seems easier.
+
+# Step 1: adding a header to the csv ----------------
