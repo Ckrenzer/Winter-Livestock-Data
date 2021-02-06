99bfdbe38a760fc9b137d1d848730fb612e6fe0a
Ckrenzer <crkrenz72@gmail.com>
2021-02-05 20:55:33 -0500

    Implemented goal 2, updated readme

--- La Junta cleaning.R
+++ La Junta cleaning.R
@@ -12,7 +12,8 @@
 # Should the header have been made beforehand and
 # therefore not need this code? Probably. That sounds
 # like a headache to implement if the file is already
-# in your system, so a separate script seems easier.
+# in your system, so a separate script seems more
+# appropriate.
 
 # Step 1: adding a header to the csv ----------------
 lajunta <- readr::read_csv("La Junta Market Reports.csv")
@@ -23,24 +24,6 @@ lajunta <- readr::read_csv("La Junta Market Reports.csv")
 
 
 # Step 2: categorizing cattle
-
-# # # KEY # # #
-# angus is "ang"
-# limousin is "lim"
-# gelbvieh is "gel"
-# charolais is "char"
-# bwf is "black white faced"
-# rwf is "red white faced"
-# wf is "white faced"
-# sim is "sims-angus"
-# lim flex is "flex limousin"
-# brah is "brahman"
-# here is "hereford"
-# clr is what is assigned when only the cattle's color is provided
-
-
-
-
 market <- read_csv("La Junta Market Reports.csv")
 market$Date <- lubridate::mdy(market$Date)
 
@@ -48,24 +31,15 @@ market$Date <- lubridate::mdy(market$Date)
 market <- market %>% 
   mutate(Reprod = str_extract(market$Type, "hfr$|str$|bull$|cow$"))
 
-# removing "x hfr", " hfr", " x str", etc. from the Type column
-market$Type <- str_remove_all(market$Type, "\\sx.*$|\\s[^\\s]*$")
-
-
-# Making a column assigning cattle reproductive status
-market <- market %>% 
-  mutate(Reprod = str_extract(market$Type, "hfr$|str$|bull$|cow$"))
-
-# removing "x hfr", " hfr", " x str", etc. from the Type column
-market$Type <- str_remove_all(market$Type, "\\sx.*$|\\s[^\\s]*$")
-
+# The bulk of the edits
 market$Type <- market %>% 
   select(Type) %>% 
   unlist() %>% 
+  str_remove_all("\\sx.*$|\\s[^\\s]*$") %>%  # removing "x hfr", " hfr", " x str", etc.
   str_replace_all("angus", "ang") %>% 
-  str_remove_all("sim-") %>%                      # removing the sim- from sim-angus, as we are not interested in this characteristic
-  str_replace_all("sim\\s+ang", "ang") %>% 
-  str_replace_all("\\s+sim", " ang") %>% 
+  str_remove_all("sim-") %>%                      # removing the sim- from sim-angus
+  str_replace_all("sim\\s+ang", "ang") %>%        # removing "sim " from "sim angus"
+  str_replace_all("\\s+sim", " ang") %>%          # replacing " sim" with " ang"
   str_replace_all("limousin", "lim") %>% 
   str_replace_all("limo", "lim") %>%              # replacing "limo" with "lim"
   str_replace_all(".*\\s+lim|lim flex", "lim") %>%# combining all different types of limousins 
@@ -75,21 +49,14 @@ market$Type <- market %>%
   str_replace_all("brahman", "brah") %>%
   str_remove_all("wf-|bwf-|rwf-") %>%             # removing face types from the data when there is other info
   str_remove_all("\\s*&") %>%                     # removing ampersands
-  str_replace_all("\\s?blk\\s?", "black") %>% 
+  str_replace_all("\\s?blk\\s?", "black") %>%     # replacing abbreviation "blk" with "black"
   str_replace_all("red black", "black red") %>%   # reordering "red and black" to "black and red"
   str_replace_all(".*-.*", "mix") %>%             # replaces any named cross-breed with "mix"
-  str_replace_all("balancer", "mix")              # replaces "balancer" with "mix", since it's a hybrid
-
-
-# Ok, I am making the executive decision to remove the color on all cattle that have more information
-# I am also going to recode "here" and "brah" as "red", since they have a very small sample size (and are very heavy)
-market$Type <- market %>% 
-  select(Type) %>% 
-  unlist() %>% 
+  str_replace_all("balancer", "mix") %>%          # replaces "balancer" with "mix", since it's a hybrid
   str_remove_all("\\s+wf|\\s+bwf|\\s+rwf") %>%    # removing face types at after the name
-  str_replace_all("^wf$", "bwf") %>%              # recoding "wf" as "bwf" because it performs similarly and has very few observations
+  str_replace_all("^wf$", "bwf") %>%              # recoding "wf" as "bwf" because it performs similarly and has few observations
   str_remove_all("wf\\s+|bwf\\s+|rwf\\s+") %>%    # removing face types before the name
-  str_replace_all("here|brah", "red") %>%         # replacing "here" and "brah" with "red"
+  str_replace_all("here|brah", "red") %>%         # replacing "here" and "brah" with "red" due to few observations and similar weight
   str_replace_all("black\\s+(ang)", "\\1") %>%    # removing color starts on this line
   str_replace_all("black\\s+(lim)", "\\1") %>% 
   str_replace_all("black\\s+(gel)", "\\1") %>% 
@@ -104,33 +71,12 @@ market$Type <- market %>%
   str_trim()
 
 
-#combining all colors into a new category--"clr"
+#combining all colors into a new category--"clr" (optional)
 market$Type <- market %>% 
   select(Type) %>% 
   unlist() %>% 
   str_replace_all("black red|black|red", "clr") %>%   # "black red," "black," and "red" are all 
   str_replace_all("bwf|rwf|wf", "face") %>%
-  str_replace_all("face", "clr")                  # remove this line if you want to stop pooling the face only group with the color only group
+  str_replace_all("face", "clr")                  # remove this line if you want to stop pooling the face group with the color group
 
 # We now have six categories in the "Type" variable
-
-# heifers are between one and two years old and have has not given birth.
-# Bred heifers are pregnant and have not given birth yet.
-# Cows have been mothers already.
-# a heifer is a female, steer is a castrated male
-# steers are bulls that have been castrated
-# the x means nothing
-
-# the hyphen means a cross breed (a mix)
-# angus cross limousine
-# saler is a breed of cattle
-# sims stands for sims angus, a type of angus
-
-# bwf stands for black wwhite face (60 days of no
-# red white face
-# flex limousine is a lim-flex
-# white face is wf
-# balancer is a combination of gelbvieh and angus
-# brah is brahman cattle
-
-# These are sales data
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Winter-Livestock-Data
-Collects data from Winter Livestock La Junta Sale Tuesday reports each week and compiles them into a csv for public use.
+Collects data from Winter Livestock La Junta Sale Tuesday reports each week and organizes them into a csv.
 
-
+This is sales data.
 
 ### UPDATE ------------------------------------------------------
 Winter Livestock changed their market report format for La Junta, CO. The "La Junta.R" script works well for market reports with the old format, in which the text looks well spaced with paragraphs on your browser. For those market reports that have no spacing between the values and look like one really long paragraph, use the "La Junta long format.R" script.
@@ -9,3 +9,8 @@ Winter Livestock changed their market report format for La Junta, CO. The "La Ju
 The version that is just one giant paragraph has no "\r" characters, so we have to use newline characters in the strsplit() call instead. That's the only difference between the files. Due to the way it is written, the "\r" is needed in the original format (the format "La Junta.R" deals with) and "\n" is needed in the long format.
 
 Could that difference easily get turned into a function, or could both R scripts get thrown into different code chunks of the same Rmd file? Sure. But I think using two scripts is easier for the user. Feel free to disagree.
+
+
+Further, there is now a script ("La Junta cleaning.R") that formats the data so that you no longer have to manually enter the column names each time you create a new file with these scripts.
+
+In addition, the 99 categories of cattle are reduced to just six! Many types of cattle have very few observations, so they were lumped in with other groups.
