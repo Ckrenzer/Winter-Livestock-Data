21e2df753d0ab8aae52936704e6716c263f8f5d6
Ckrenzer <crkrenz72@gmail.com>
2021-08-14 00:46:50 -0400

    simplified comments

--- Collection/functions/determine_date_of_sale().R
+++ Collection/functions/determine_date_of_sale().R
@@ -13,11 +13,9 @@ determine_date_of_sale <- function(livestock_data = livestock_data){
   ##### CASE 1: The date is in "month ##th YYYY" format (Ex. "march 9th 2021")
   #note: some sales take place over two days, so we have to remove the "& ##th" following the first day's date
   date_of_sale <- str_extract(date_of_sale_sentence, paste0(month_of_sale, "\\s+\\d{1,2}[a-z0-9&\\s]{0,15}\\d{4}")) %>% 
-    # replacing the month name with its corresponding number then
-    # removing letters and replacing space characters with hyphens
     month_name_to_num(text = .) %>% 
     str_remove_all("[a-z]") %>%
-    str_replace_all("\\s+", " ") %>% #replacing multiple spaces with just one space
+    str_replace_all("\\s+", " ") %>% 
     str_replace_all("\\s", "-")
   
   
@@ -29,15 +27,12 @@ determine_date_of_sale <- function(livestock_data = livestock_data){
     # Pulling out the part of the sentence containing the date
     date_of_sale <- str_extract(date_of_sale_sentence, paste0(month_of_sale, "\\s+\\d{1,2}.{0,40}\\d{4}"))
     
-    #Pulling out the month and day
-    md <- str_extract(date_of_sale, paste0(month_of_sale, "\\s+\\d{1,2}"))
-    #Pulling out the year
-    y <- str_extract(date_of_sale, "\\d{4}")
-    
-    # Changing the month to its corresponding number
-    md <- month_name_to_num(text = md) %>% 
-      #replacing the date with a hyphen
+    # month and day
+    md <- str_extract(date_of_sale, paste0(month_of_sale, "\\s+\\d{1,2}")) %>% 
+      month_name_to_num(text = .) %>% 
       str_replace_all("\\s+", "-")
+    # year
+    y <- str_extract(date_of_sale, "\\d{4}")
     
     #we only want to do this step if date_of_sale is not null (we will get a string saying "NA-NA" otherwise,
     #which is a pain to correct for in subsequent case-checking)
@@ -51,7 +46,7 @@ determine_date_of_sale <- function(livestock_data = livestock_data){
   
   ##### CASE 3: The year was not provided
   if(is.na(lubridate::mdy(date_of_sale))){
-   
+    
     #Pulling out the month and day
     md <- str_extract(date_of_sale_sentence, paste0(month_of_sale, "\\s+\\d{1,2}"))
     #Setting the year equal to the previous sale's year
