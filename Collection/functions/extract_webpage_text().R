b036fcd534eb96aaf8208c315b2b412d6f52bc4d
Ckrenzer <crkrenz72@gmail.com>
2021-08-14 15:37:49 -0400

    changed parameter name

--- Collection/functions/extract_webpage_text().R
+++ Collection/functions/extract_webpage_text().R
@@ -1,6 +1,6 @@
-extract_webpage_text <- function(URL = URL){
+extract_webpage_text <- function(url = URL){
   # The HTML elements are consistent across webpages
-  livestock_data <- read_html(URL) %>% 
+  livestock_data <- read_html(url) %>% 
     html_nodes("div:nth-child(9) div.sml") %>% 
     html_text() %>% 
     return()
