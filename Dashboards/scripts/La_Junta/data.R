67b3dea8f70cf183b0ce736fb9c8d1643984221c
Ckrenzer <crkrenz72@gmail.com>
2021-08-20 11:32:41 -0400

    moved data import to a dedicated script

---
+++ Dashboards/scripts/La_Junta/data.R
@@ -0,0 +1,39 @@
+# We load in the app's data using this script
+
+# Reading in the file and removing the URL column
+# Fortunately, we only have to do this once
+lajunta <- readr::read_csv("https://raw.githubusercontent.com/Ckrenzer/Winter-Livestock-Data/main/La%20Junta%20Market%20Reports.csv",
+                           col_types = readr::cols(Date = col_date("%m-%d-%Y"),
+                                                   Buyer = readr::col_factor(),
+                                                   Quantity = readr::col_double(),
+                                                   Type = readr::col_factor(),
+                                                   Weight = readr::col_double(),
+                                                   Price = readr::col_double(),
+                                                   URL = readr::col_character(),
+                                                   Reprod = readr::col_factor())) %>% 
+  dplyr::select(-URL)
+
+
+# The full, unfiltered dataset for export
+lajunta_full <- lajunta
+
+
+lajunta <- lajunta %>% 
+  dplyr::filter(!is.na(Reprod))
+
+# This is meant to speed up the runtime instead of placing it in multiple reactive functions
+outliers_removed <- lajunta %>% 
+  filter(Price < 475)
+
+# recodes bulls that act like steers on the market as steers
+# and similarly for cows that act like heifers
+model_data <- outliers_removed %>% 
+  dplyr::select(-Buyer) %>% 
+  mutate(Price = log(Price),
+         Reprod = as.character(Reprod),
+         Reprod = case_when(
+           Reprod == "bull" && Weight < 1050 ~ "str",
+           Reprod == "cow" && Price > 100 ~ "hfr",
+           TRUE ~ Reprod),
+         Reprod = as.factor(Reprod)
+  )
